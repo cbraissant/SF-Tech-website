f7317a93ad866fc59c3d5e445d5463c6ccc3c8ad
cbraissant <chrisbraissant@gmail.com>
2021-12-08 11:53:28 +0100

    Typing mistake

--- _src/js/script.js
+++ _src/js/script.js
@@ -11,55 +11,55 @@ const menu = document.querySelectorAll('.menu-dropdown');
 const getSiblings = n => [...n.parentElement.children].filter(c => c != n);
 // Loop through each menu
 menu.forEach(item => {
-  // Add a click event
-  item.addEventListener('click', e => {
-    // Toggle the active element
-    e.currentTarget.classList.toggle('is-active');
-    // Get the siblings elements
-    const siblings = getSiblings(e.currentTarget);
-    // Loop through the siblings and remove the active class
-    siblings.forEach(sib => sib.classList.remove('is-active'));
-  });
+    // Add a click event
+    item.addEventListener('click', e => {
+        // Toggle the active element
+        e.currentTarget.classList.toggle('is-active');
+        // Get the siblings elements
+        const siblings = getSiblings(e.currentTarget);
+        // Loop through the siblings and remove the active class
+        siblings.forEach(sib => sib.classList.remove('is-active'));
+    });
 });
 
 // Toggle the menu when the icon is clicked
 // **************************************************
 document.querySelector('.menu-icon').addEventListener('click', () => {
-  document.querySelector('body').classList.toggle('menu-is-open');
+    document.querySelector('body').classList.toggle('menu-is-open');
 });
 
 // Disable enter key to submit form
 // **************************************************
 document.addEventListener('keydown', e => {
-  e.keyCode == 13 && e.target == document.querySelector('form input')
-    ? e.preventDefault()
-    : null;
+    e.keyCode == 13 && e.target == document.querySelector('form input')
+        ? e.preventDefault()
+        : null;
 });
 
 // Disable all active elements on resize
 // **************************************************
 window.onresize = () => {
-  document.querySelector('body').classList.remove('menu-is-open');
-  document
-    .querySelectorAll('.is-active')
-    .forEach(item => item.classList.remove('is-active'));
+    document.querySelector('body').classList.remove('menu-is-open');
+    document
+        .querySelectorAll('.is-active')
+        .forEach(item => item.classList.remove('is-active'));
 };
 
 // Change the size of the header when the window is scrolled
 // **************************************************
 const masthead = document.querySelector('#masthead');
 window.addEventListener('scroll', () => {
-  window.scrollY > 200
-    ? masthead.classList.add('is-scroll')
-    : masthead.classList.remove('is-scroll');
+    window.scrollY > 200
+        ? masthead.classList.add('is-scroll')
+        : masthead.classList.remove('is-scroll');
 });
 
 // hide the scroll wheel when the window is scrolled
 // **************************************************
 window.addEventListener('scroll', () => {
-  window.scrollY > 50
-    ? document.querySelector('#icon-scroll').classList.add('is-scroll')
-    : null;
+    window.scrollY > 50
+        ? document.querySelector('#icon-scroll').classList.add('is-scroll')
+        : null;
 });
 
 // **************************************************
@@ -121,15 +121,15 @@ window.addEventListener('scroll', () => {
 
 // CSS animation on scroll
 AOS.init({
-  duration: 800
+    duration: 800
 });
 
 // ScrollReveal - Reveal items on scroll
 ScrollReveal().reveal('.jsReveal', {
-  delay: 0,
-  duration: 300,
-  scale: 0.0,
-  reset: true
+    delay: 0,
+    duration: 300,
+    scale: 0.0,
+    reset: true
 });
 
 // Disable the drag and drop of the button class
@@ -147,21 +147,21 @@ let headerOffset = document.querySelector('header').offsetHeight;
 let anchorlinks = document.querySelectorAll('a[href^="#"]');
 
 for (let item of anchorlinks) {
-  item.addEventListener('click', e => {
-    let hashval = item.getAttribute('href');
-    let target = document.querySelector(hashval);
-    let elementPosition = target.getBoundingClientRect().top;
-    console.log('begining of paragraph: ' + elementPosition);
-    let actualPosition = window.scrollY;
-    console.log('actual position: ' + actualPosition);
-    var offsetPosition = elementPosition - headerOffset + actualPosition;
-    console.log('offset to scroll: ' + offsetPosition);
-    window.scrollTo({
-      top: offsetPosition,
-      behavior: 'smooth'
+    item.addEventListener('click', e => {
+        let hashval = item.getAttribute('href');
+        let target = document.querySelector(hashval);
+        let elementPosition = target.getBoundingClientRect().top;
+        console.log('begining of paragraph: ' + elementPosition);
+        let actualPosition = window.scrollY;
+        console.log('actual position: ' + actualPosition);
+        var offsetPosition = elementPosition - headerOffset + actualPosition;
+        console.log('offset to scroll: ' + offsetPosition);
+        window.scrollTo({
+            top: offsetPosition,
+            behavior: 'smooth'
+        });
+        e.preventDefault();
     });
-    e.preventDefault();
-  });
 }
 
 //  JQuery animate as replacement
